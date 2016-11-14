0efebebb78653768171197f645274209001c0a50
jmbeach <jaredbeachdesign@gmail.com>
2016-11-14 09:02:33 -0600

    Remove Convention

--- Models/ExampleContext.cs
+++ Models/ExampleContext.cs
@@ -14,9 +14,10 @@ namespace YourNamespace.Models
         public DbSet<ExampleModel> ExampleModels { get; set; }
 
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        protected override void OnModelCreating(System.Data.Entity.DbModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Example>().HasKey(e => e.Example2ID).Property(a => a.ExampleID).HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+            modelBuilder.Conventions.Remove<System.Data.Entity.ModelConfiguration.Conventions.PluralizingTableNameConvention>();
             modelBuilder.Entity<Example2>().HasRequired(e => e.ExampleID).WithMany(c => c.Example).WillCascadeOnDelete(false);
         }
     }
